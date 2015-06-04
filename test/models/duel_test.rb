b109d5591e6b1a01d7fa472c6632b6b2920a32b1
Jevon Wright <jevon@powershop.co.nz>
2015-06-04 13:19:44 +1200

    Add tests for second player starting first

--- test/models/duel_test.rb
+++ test/models/duel_test.rb
@@ -1,12 +1,9 @@
 require 'test_helper'
 
 class DuelTest < ActiveSupport::TestCase
-  def default_duel
-    Duel.new({ first_player: 1, current_player: 1, phase: 1, priority_player: 1 })
-  end
 
   test "passing order works as expected" do
-    @duel = default_duel
+    @duel = Duel.new({ first_player: 1, current_player: 1, phase: 1, priority_player: 1 })
     assert_equal 4, @duel.total_phases
 
     [
@@ -26,10 +23,57 @@ class DuelTest < ActiveSupport::TestCase
       [2, 3, 1],
       [2, 4, 2],
       [2, 4, 1],
-      # and then back again
-      [1, 1, 1]
     ].each do |test|
       assert_equal test, [@duel.current_player, @duel.phase, @duel.priority_player]
+      assert_equal 1, @duel.turn, "at turn #{test}"
+      @duel.pass
+    end
+
+    [
+      # and then back again
+      [1, 1, 1],
+      [1, 1, 2],
+    ].each do |test|
+      assert_equal test, [@duel.current_player, @duel.phase, @duel.priority_player]
+      assert_equal 2, @duel.turn, "at turn #{test}"
+      @duel.pass
+    end
+  end
+
+  test "passing order works as expected when the second player starts first" do
+    @duel = Duel.new({ first_player: 2, current_player: 2, phase: 1, priority_player: 2 })
+    assert_equal 4, @duel.total_phases
+
+    [
+      [2, 1, 2],
+      [2, 1, 1],
+      [2, 2, 2],
+      [2, 2, 1],
+      [2, 3, 2],
+      [2, 3, 1],
+      [2, 4, 2],
+      [2, 4, 1],
+      [1, 1, 1],
+      [1, 1, 2],
+      [1, 2, 1],
+      [1, 2, 2],
+      [1, 3, 1],
+      [1, 3, 2],
+      [1, 4, 1],
+      [1, 4, 2],
+    ].each do |test|
+      assert_equal test, [@duel.current_player, @duel.phase, @duel.priority_player]
+      assert_equal 1, @duel.turn, "at turn #{test}"
+      @duel.pass
+    end
+
+    [
+      # and then back again
+      [2, 1, 2],
+      [2, 1, 1],
+    ].each do |test|
+      assert_equal test, [@duel.current_player, @duel.phase, @duel.priority_player]
+      assert_equal 2, @duel.turn, "at turn #{test}"
       @duel.pass
     end
   end
