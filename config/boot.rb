108c31c0dc1bb3ae0c4ec57fff68bc011c0cedb0
Jevon Wright <jevon@powershop.co.nz>
2015-06-04 14:40:45 +1200

    We don't need to load card definitions at boot

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,3 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-
-# Load all card definitions
-Dir[File.dirname(__FILE__) + '/../app/cards/**/*.rb'].each {|file| require file }
