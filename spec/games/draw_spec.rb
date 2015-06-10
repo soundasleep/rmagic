d7e50b1ff4190db07ade24a25ae4657088aafecf
Jevon Wright <jevon@powershop.co.nz>
2015-06-10 15:25:18 +1200

    Move drawing and passing tests to Rspec

---
+++ spec/games/draw_spec.rb
@@ -0,0 +1,48 @@
+require_relative "game_test"
+
+RSpec.describe "Defending" do
+  before :each do
+    setup
+  end
+
+  it "starting a new turn draws a card" do
+    expect(@duel.player1.hand.count).to eq(0)
+    expect(@duel.player2.hand.count).to eq(0)
+
+    @duel.phase_number = @duel.total_phases
+    @duel.current_player_number = 2
+    @duel.priority_player_number = 2
+
+    @duel.pass
+
+    expect(@duel.player1.hand.count).to eq(0)
+    expect(@duel.player2.hand.count).to eq(0)
+
+    @duel.pass
+
+    expect(@duel.phase_number).to eq(1)
+    expect(@duel.player1.hand.count).to eq(1)
+    expect(@duel.player2.hand.count).to eq(0)
+  end
+
+  it "starting a new turn draws a card for the other player" do
+    expect(@duel.player1.hand.count).to eq(0)
+    expect(@duel.player2.hand.count).to eq(0)
+
+    @duel.phase_number = @duel.total_phases
+    @duel.current_player_number = 1
+    @duel.priority_player_number = 1
+
+    @duel.pass
+
+    expect(@duel.player1.hand.count).to eq(0)
+    expect(@duel.player2.hand.count).to eq(0)
+
+    @duel.pass
+
+    expect(@duel.phase_number).to eq(1)
+    expect(@duel.player1.hand.count).to eq(0)
+    expect(@duel.player2.hand.count).to eq(1)
+  end
+
+end
