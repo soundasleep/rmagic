d499d1f1c773dd99c295e13aeaa2e7cac75efbc1
Jevon Wright <jevon@powershop.co.nz>
2015-06-25 12:39:05 +1200

    Cache loaded cards in the card universe

--- app/services/card_universe.rb
+++ app/services/card_universe.rb
@@ -1,9 +1,17 @@
 class CardUniverse
+  def initialize
+    @cache = {}
+  end
+
   def library
     @library ||= Library.new
   end
 
   def find_metaverse(metaverse_id)
+    @cache[metaverse_id] ||= load_card(metaverse_id)
+  end
+
+  def load_card(metaverse_id)
     if library.card_types.has_key? metaverse_id
       library.card_types[metaverse_id].new
     else
