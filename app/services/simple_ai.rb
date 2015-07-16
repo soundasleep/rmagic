4ee6c184d0ea69f18181e9cbcddfab12891d2390
Jevon Wright <jevon@powershop.co.nz>
2015-07-17 11:37:51 +1200

    The AI now always plays and uses as many cards as possible

--- app/services/simple_ai.rb
+++ app/services/simple_ai.rb
@@ -1,8 +1,23 @@
 class SimpleAI
+  attr_reader :duel
+  attr_reader :player
+
   def do_turn(duel, player)
+    @duel = duel
+    @player = player
+
+    # play as many cards as possible
+    while playable_cards.any? || ability_cards.any? do
+      if playable_cards.any?
+        playable_cards.first.do duel
+      end
+      if ability_cards.any?
+        ability_cards.first.do duel
+      end
+    end
+
     # always declare attackers always always
-    attackers = action_finder(duel).available_attackers(player)
-    DeclareAttackers.new(duel: duel, zone_cards: attackers).call
+    DeclareAttackers.new(duel: duel, zone_cards: available_attackers).call
 
     # really simple AI: we just pass
     PassPriority.new(duel: duel).call
@@ -10,8 +25,20 @@ class SimpleAI
 
   private
 
-    def action_finder(duel)
-      ActionFinder.new(duel)
+    def playable_cards
+      action_finder.playable_cards(player)
+    end
+
+    def ability_cards
+      action_finder.ability_cards(player)
+    end
+
+    def available_attackers
+      action_finder.available_attackers(player)
+    end
+
+    def action_finder
+      @action_finder ||= ActionFinder.new(duel)
     end
 
 end
