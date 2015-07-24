c622076ac0dccb906c30f416225ddad71e4b59fc
Jevon Wright <jevon@powershop.co.nz>
2015-07-24 14:04:35 +1200

    Add test that the SimpleAI can pass ten times in a row
    This test can also be used to evaluate performance and profiling

---
+++ spec/ai/ai_passing_spec.rb
@@ -0,0 +1,29 @@
+require "game_helper"
+
+# This test allows us to both test that the simple AI will always pass,
+# and as a way to test performance.
+RSpec.describe SimpleAI, type: :ai do
+  let(:duel) { create_game }
+
+  before :each do
+    duel.mulligan_phase!
+
+    10.times { create_order_card player1.deck, Library::Metaverse1, player1.next_deck_order }
+    10.times { create_order_card player2.deck, Library::Metaverse1, player2.next_deck_order }
+
+    player2.update! is_ai: true
+  end
+
+  (0..10).each do |n|
+    context "after #{n} passes" do
+      before :each do
+        n.times { pass_priority }
+      end
+
+      it "is our turn" do
+        expect(duel.priority_player).to eq(player1)
+      end
+    end
+  end
+
+end
