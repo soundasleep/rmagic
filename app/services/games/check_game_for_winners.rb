f994910bbc479aef37755a06061c8b081a9233a5
Jevon Wright <jevon@powershop.co.nz>
2015-07-29 15:46:38 +1200

    When a player has zero life, they lose the game

--- app/services/games/check_game_for_winners.rb
+++ app/services/games/check_game_for_winners.rb
@@ -6,6 +6,17 @@ class CheckGameForWinners
   end
 
   def call
+    # all players with zero life have lost
+    duel.players.select { |p| p.life <= 0 }.each do |player|
+      if !player.lost?
+        # update the player as lost (once)
+        player.update! lost: true, final_turn: duel.turn
+
+        # add an action log
+        duel.action_logs.create! global_action: "lost", player: player
+      end
+    end
+
     # if there is only one player left, that player has won, and the game has ended
     remaining = duel.players.select{ |p| p.in_game? }
     if remaining.length == 1
